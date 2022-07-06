9f3ed6d070c62bc334a1023a6712973c3fb50d41
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-07-06 11:49:02 +0530

    :plus: and :sparkles: added deps and updated env

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,21 +1,26 @@
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_POLYGON_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 100,
+    enabled: process.env.REPORT_GAS ? true : false,
+  },
 };
+
