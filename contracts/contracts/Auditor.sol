ce3e141734a70c89903cfa3429af30584c76f082
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-07-16 11:43:45 +0530

    :construction: update

--- contracts/contracts/Auditor.sol
+++ contracts/contracts/Auditor.sol
@@ -6,11 +6,18 @@ contract Auditor {
 	struct Audit {
 		address creator;
 		address contractAddress;
-		uint256 timestamp;
 		address[] jury;
+		uint256 timestamp;
+		uint256 totalYesPool;
+		uint256 totalNoPool;
 		mapping(address => uint256) yesPool;
 		mapping(address => uint256) noPool;
-		mapping(address => bool) verdict;
+		mapping(address => Bug) bugs;
+	}
+
+	struct Bug {
+		uint256 timestamp;
+		bool verdict;
 	}
 
 	Audit[] audits;
@@ -47,14 +54,104 @@ contract Auditor {
 		address indexed voter,
 		uint256 totalNoPool
 	);
+	event NewBugReported(
+		address indexed contractAddress,
+		address indexed reporter,
+		uint256 timestamp
+	);
 
 	modifier onlyOwner() {
 		require(msg.sender == owner, "Only accessible by owner!");
 		_;
 	}
 
+	modifier equallyFunded() {
+		require(msg.value > 0 && msg.value % 2 == 0, "Must be equally funded!");
+		_;
+	}
+
 	constructor() {
 		owner = payable(msg.sender);
+	}
+
+	function createAudit(address contractAddress) external equallyFunded {
+		Audit newAudit = Audit({
+			creator: msg.sender,
+			contractAddress: contractAddress,
+			timestamp: block.timestamp,
+			totalYesPool: msg.value / 2,
+			totalNoPool: msg.value / 2,
+			jury: []
+		});
+		newAudit.yesPool[msg.sender] = newAudit.totalYesPool;
+		newAudit.noPool[msg.sender] = newAudit.totalNoPool;
+		audits.push(newAudit);
+
+		emit AuditRequested(msg.sender, contractAddress, block.timestamp);
+	}
+
+	function fundNoBugs(address contractAddress) external {
+		audits[contractAddress].totalNoPool += msg.value;
+		audits[contractAddress].noPool[msg.sender] = msg.value;
+
+		emit AuditNoPoolUpdated(
+			contractAddress,
+			msg.sender,
+			audits[contractAddress].totalNoPool
+		);
+	}
+
+	function reportBug(address contractAddress) external {
+		Bug newBug = Bug({
+			timestamp: block.timestamp,
+			verdict: false
+		});
+
+		audits[contractAddress].bugs[msg.sender] = newBug;
+
+		emit NewBugReported(
+			contractAddress,
+			msg.sender,
+			block.timestamp,
+		);
+
+		audits[contractAddress].totalYesPool += msg.value;
+		audits[contractAddress].yesPool[msg.sender] = msg.value;
+
+		emit AuditYesPoolUpdated(
+			contractAddress,
+			msg.sender,
+			audits[contractAddress].totalYesPool
+		);
+	}
+
+	function juryVerdict(address contractAddress, address reporter, bool verdict) {
+		audits[contractAddress].bugs[reporter].verdict = verdict;
+		if (verdict) {
+			uint256 noPool = audits[contractAddress].totalNoPool;
+			uint256 yesPool = audits[contractAddress].totalYesPool;
+
+			uint256 bet = audits[contractAddress].yesPool[reporter];
+			uint256 reporterReward = ((bet * 100) / yesPool) * (yesPool + noPool);
+
+			for (address voter in audits[contractAddress].yesPool) {
+				if (voter != reporter) {
+					// uint256 voterReward = 
+					// review voter reward logic
+				}
+			}
+
+			yesPool += noPool;
+			audits[contractAddress].totalNoPool = 0;
+			uint256 juryReward = (yesPool * 5) / 100;
+
+			address[] jury = audits[contractAddress].jury;
+			for (uint256 i = 0; i < jury.length; i++) {
+				jury.transfer(juryReward / 5); // review for reentrancy
+			}
+			yesPool -= (yesPool * 5) / 100; // review for gas
+
+		}
 	}
 
 	function transferOwnership(address payable _owner) external onlyOwner {
