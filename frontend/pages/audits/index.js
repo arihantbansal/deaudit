476e2c1c030aaad8ad1b4a73950715648c4d4cde
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2022-08-09 18:41:33 +0530

    added lodash

--- frontend/pages/audits/index.js
+++ frontend/pages/audits/index.js
@@ -10,12 +10,34 @@ const audits = () => {
       name: "AuditDAO",
       address: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
       creator: "0x3b5e3af9c9c6c5dcd09cef8f3d3c9c74d9ac2a2a",
-      bugs: 2,
+      juryMembers: [
+        "0xA0C6dFF0CD9d034Ddb9fE46F9B9AeFbeC9EA4358F",
+        "0x95Fce0ECfc530cfbfaA70D8644a8De8E12De723e",
+        "0xA0C6dFF0CD9d034Ddb9fE46F9B9AeFbeC9EA4358F",
+      ],
+      bugs: [
+        {
+          userAddress: "0xA0C6dFF0CD9d034Ddb9fE46F9B9AeFbeC9EA4358F",
+          description: "Not ownable",
+          verified: true,
+        },
+        {
+          userAddress: "0x95Fce0ECfc530cfbfaA70D8644a8De8E12De723e",
+          description: "Can use OpenZeppelin to ensure reusability",
+          verified: false,
+        },
+        {
+          userAddress: "0xaC6dFF0CD9d034Ddb9fE46F9B9AeFbeC9EA4358F",
+          description: "Would be better to implement ERC1155 instead of 721",
+          verified: true,
+        },
+      ],
       createdAt: "2020-01-01",
       poolSizes: {
         NoBug: "5.3",
         YesBug: "2.1",
       },
+      tags: ["erc721", "polygon"],
     },
   ];
   return (
